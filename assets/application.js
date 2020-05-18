74f8a3341909ad9becc241388f0ae92b28796703
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2020-05-18 17:11:46 +0900

    Add modal on product page

--- assets/application.js
+++ assets/application.js
@@ -1,5 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const welcomeModal = document.getElementById('welcomeModal');
-  welcomeModal.classList.remove("hide");
-  console.log('HI')
-})
+// document.addEventListener('DOMContentLoaded', function() {
+//   const welcomeModal = document.getElementById('welcomeModal');
+//   welcomeModal.classList.remove("hide");
+//   }
+// )
+$(window).on('load',function(){
+  $('#myModal').removeClass('hide').addClass('show')
+  $('.close-btn').click(function(){
+    $('#myModal').removeClass('show').addClass('hide')
+  });
+});
+
