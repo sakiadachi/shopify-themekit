8ad3ccab1bdda53d3252001ab66b3f58c098266b
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2020-05-20 14:52:57 +0900

    Last commit before submittion
    Clean code and style
    Check over code with requirements, and made correction

--- assets/application.js
+++ assets/application.js
@@ -1,43 +1,10 @@
+// Opens Modal when the user access on the product page (c)
 $(window).on('load',function(){
     $('#product-modal').removeClass('hide').addClass('show')
     $('.close-btn').click(function(){
         $('#product-modal').removeClass('show').addClass('hide')
     });
+    // Print the value of "Random Title" on console (b.i.)
     const randomText = $('.random-title').html();
         console.log("Random title: " + randomText);
-});
-
-// function addItemToCart(event){
-//     event.preventDefault();
-
-//     jQuery.ajax({
-//             type: 'POST',
-//             url: '/cart/add.js',
-//             data: $('#'+'add-to-cart-form').serialize(),
-//             dataType: 'json',
-//             success: function() {
-//                 updateCartItem();
-//             },
-//     });
-// }
-// function updateCartItem(e) {
-//     const addItemModal = document.getElementById("new-cart-item");
-//     addItemModal.style.display = "inherit";
-// }
-// const span = document.getElementsByClassName("modal-close");
-// span.addEventListener('click', closeModal);
-
-// function closeModal() {
-//     const addItemModal = document.getElementById("new-cart-item");
-//     addItemModal.style.display = "none";
-// }
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     const addToCartButton = document.getElementById("add-to-cart-btn");
-//     const stock = "{{ product.variants.first.inventory_quantity }}";
-//     if(stock > 0) {
-//         addToCartButton.classList.add("instock");
-//     } else {
-//         addToCartButton.classList.add("nostock");
-//     }
-//     console.log(stock)
-// });
+})
